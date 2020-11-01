ebe0b7f441680f75373746fd18db61454a0fc39d
anh-pt <65848104+anh-pt@users.noreply.github.com>
2020-11-01 12:53:45 +0700

    add authur

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php 
 
-Route::get('/','AdminController@index')->name('dashboard');
-Route::get('/Customer','CustomerController@index')->name('customer');
-Route::get('/Customer/add','CustomerController@add')->name('customer.add');
+Route::get('/dashdoard','AdminController@index')->name('dashboard');
+Route::get('/customer','CustomerController@index')->name('customer');
+Route::get('/customer/add','CustomerController@add')->name('customer.add');
+Route::get('/authur','AuthurController@index')->name('authur');
 ?>
