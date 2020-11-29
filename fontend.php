1b45c098db7d48a1fe3418939b3dec52da2af197
anh-pt <65848104+anh-pt@users.noreply.github.com>
2020-11-29 10:48:12 +0700

    font

---
+++ fontend.php
@@ -0,0 +1,280 @@
+[33mcommit dd7a99b0ca938832f21e5fe46bcd4d56da9770ad[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Merge: 9768106f9 69e80ef68
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sun Nov 29 10:31:38 2020 +0700
+
+    Merge branch 'anhpt' into main
+
+[33mcommit 69e80ef68d9e80fe3b8022db59f5e06372370a85[m[33m ([m[1;31morigin/anhpt[m[33m, [m[1;32manhpt[m[33m)[m
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sun Nov 29 10:24:59 2020 +0700
+
+    fontend
+
+[33mcommit 9768106f93ecfeed5caa3f6b62207a51a2b740f1[m
+Merge: 04c8a5fc4 dffe8dca0
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sat Nov 28 16:28:16 2020 +0700
+
+    Merge branch 'anhpt' into main
+
+[33mcommit dffe8dca03c4416d55b304dac799a7df66c80562[m
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sat Nov 28 16:14:44 2020 +0700
+
+    fontend
+
+[33mcommit 04c8a5fc4a568caaa688014e5833dc46d06dd72a[m
+Merge: bf277deb0 13c3adeeb
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sat Nov 28 14:05:30 2020 +0700
+
+    Merge branch 'anhpt' into main
+
+[33mcommit 13c3adeeb1fc7e84dc0e8222b9b469a395c87838[m
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sat Nov 28 13:39:52 2020 +0700
+
+    edit theme
+
+[33mcommit dad7951955da7fb9f5f51a7b5b5614202355697f[m
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sat Nov 28 12:23:13 2020 +0700
+
+    update
+
+[33mcommit f2f9b26c02bed07f4b18d161762994ca7e12ba87[m
+Merge: 400aeab69 46d70811a
+Author: leduchoanh04062k <68231217+leduchoanh04062k@users.noreply.github.com>
+Date:   Wed Nov 11 01:30:55 2020 +0700
+
+    Merge pull request #38 from manhnd3-07/hoanh2
+    
+    Hoanh2
+
+[33mcommit 46d70811aa3beda61c828ccbf7af34f57ffd973f[m[33m ([m[1;31morigin/hoanh2[m[33m)[m
+Merge: f3da2ffe8 400aeab69
+Author: leduchoanh04062k <leduchoanh2k@gmail.com>
+Date:   Wed Nov 11 01:29:31 2020 +0700
+
+    add_calamviec
+
+[33mcommit f3da2ffe83ca1fc155be71e939c1d8461d506dbf[m
+Author: leduchoanh04062k <leduchoanh2k@gmail.com>
+Date:   Wed Nov 11 01:12:48 2020 +0700
+
+    add_calamviec
+
+[33mcommit 1c0e5af9f78f0c3ab80e38d990a49529304fe716[m
+Author: leduchoanh04062k <leduchoanh2k@gmail.com>
+Date:   Wed Nov 11 00:54:27 2020 +0700
+
+    add_calamviec
+
+[33mcommit 400aeab69a582dffe66a8784b724f3e1aba13319[m
+Merge: 5a96b2724 35e19bb08
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Mon Nov 9 23:34:10 2020 +0700
+
+    Merge pull request #35 from manhnd3-07/anhpt
+    
+    ưt
+
+[33mcommit 35e19bb086e992bc0ac824776dcaa2c30cbf3df7[m
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Mon Nov 9 23:33:16 2020 +0700
+
+    ưt
+
+[33mcommit 5a96b27248a54fa7b003eb9ec1fe3825f7643ec3[m
+Merge: a26daa868 c9fed05e0
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Mon Nov 9 01:08:20 2020 +0700
+
+    Merge pull request #34 from manhnd3-07/manhnd
+    
+    phan quyen
+
+[33mcommit c9fed05e0835f8340b658f14f52e5aee0b2b0118[m
+Author: manhnd3 <manhnd3@vnext.com.vn>
+Date:   Mon Nov 9 01:07:34 2020 +0700
+
+    phan quyen
+
+[33mcommit a26daa86810a4c347ecb0052122caf10dc72ffb2[m
+Merge: c7ead5210 42675dc65
+Author: tiennhph <72286865+tiennhph@users.noreply.github.com>
+Date:   Sun Nov 8 18:10:30 2020 +0700
+
+    Merge pull request #33 from manhnd3-07/tien1
+    
+    edit
+
+[33mcommit c7ead521088c6d817bf226fac037d3d898480641[m
+Merge: d5f7735f0 4f36467f6
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Sun Nov 8 17:58:28 2020 +0700
+
+    Merge pull request #32 from manhnd3-07/manhnd
+    
+    edit foder laravel
+
+[33mcommit 4f36467f685e79ad8f16b6a05f52688455fa08c7[m
+Author: manhnd3 <manhnd3@vnext.com.vn>
+Date:   Sun Nov 8 17:57:24 2020 +0700
+
+    edit foder laravel
+
+[33mcommit d5f7735f00a8c2531dd8942ae79c24429f59f5b0[m
+Merge: 6c9071600 46d028e16
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Sun Nov 8 17:28:46 2020 +0700
+
+    Merge pull request #31 from manhnd3-07/manhnd
+    
+    edit
+
+[33mcommit 46d028e1667b238b8ee01e77dddee6eceb69389e[m
+Author: manhnd3 <manhnd3@vnext.com.vn>
+Date:   Sun Nov 8 17:27:48 2020 +0700
+
+    edit
+
+[33mcommit 6c9071600afc0097a0db6395d1335ec4bfe393ca[m
+Merge: 2c91722a8 1dad43c96
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Sun Nov 8 17:08:36 2020 +0700
+
+    Merge pull request #30 from manhnd3-07/long
+    
+    Merge pull request #27 from manhnd3-07/main
+
+[33mcommit 2c91722a8e471db8743663fe1d212d5d171e9fc8[m
+Merge: a659567f3 869604756
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Sun Nov 8 17:04:44 2020 +0700
+
+    Merge pull request #29 from manhnd3-07/manhnd
+    
+    edit
+
+[33mcommit 869604756d0a4fc9a91c158019d47a86ca798e1f[m
+Merge: 95ff3d2e6 a659567f3
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Sun Nov 8 17:04:28 2020 +0700
+
+    Merge branch 'main' into manhnd
+
+[33mcommit 95ff3d2e6922db91d5ed7a409011d30e09ed4d0c[m
+Author: manhnd3 <manhnd3@vnext.com.vn>
+Date:   Sun Nov 8 17:03:04 2020 +0700
+
+    edit
+
+[33mcommit a659567f30bdca99277026d6795d17c1784c504d[m
+Merge: 6965f4324 b1a45553f
+Author: tiennhph <72286865+tiennhph@users.noreply.github.com>
+Date:   Sun Nov 8 16:21:53 2020 +0700
+
+    Merge pull request #28 from manhnd3-07/tien
+    
+    Tien
+
+[33mcommit 1dad43c96c25694c19f67833b3f5d12509611772[m
+Merge: e25f4df40 6965f4324
+Author: longtvph08895 <57783272+longtvph08895@users.noreply.github.com>
+Date:   Sun Nov 8 16:07:37 2020 +0700
+
+    Merge pull request #27 from manhnd3-07/main
+    
+    keo code
+
+[33mcommit 6965f43247e96c302c09e7be28f2b54ef809d296[m
+Merge: ef1db2b42 cfb16cc13
+Author: leduchoanh04062k <68231217+leduchoanh04062k@users.noreply.github.com>
+Date:   Sun Nov 8 16:06:33 2020 +0700
+
+    Merge pull request #26 from manhnd3-07/hoanh
+    
+    edit
+
+[33mcommit cfb16cc1365da410e9ff34673c9ab54a7b250589[m
+Merge: 17e92fba0 ef1db2b42
+Author: leduchoanh04062k <68231217+leduchoanh04062k@users.noreply.github.com>
+Date:   Sun Nov 8 16:06:26 2020 +0700
+
+    Merge branch 'main' into hoanh
+
+[33mcommit 17e92fba0fa2fdcf7e958c6f62ea7e6e596facc8[m
+Author: leduchoanh04062k <leduchoanh2k@gmail.com>
+Date:   Sun Nov 8 16:05:22 2020 +0700
+
+    edit
+
+[33mcommit ef1db2b42e986e25d511b0b6cca18c8dd6150280[m
+Merge: 6442a06c2 a92994e99
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sun Nov 8 16:01:14 2020 +0700
+
+    Merge pull request #25 from manhnd3-07/anhpt
+    
+    edit
+
+[33mcommit a92994e990dda6810f35e4a86d95ec6557cbbc6a[m
+Merge: ec78db7fb 6442a06c2
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sun Nov 8 16:01:08 2020 +0700
+
+    Merge branch 'main' into anhpt
+
+[33mcommit ec78db7fb3a2cad385b2620c2c6d2ca7f78cfdd5[m
+Author: anh-pt <65848104+anh-pt@users.noreply.github.com>
+Date:   Sun Nov 8 15:58:47 2020 +0700
+
+    edit
+
+[33mcommit 6442a06c242b5b4e2cfd6b13175c65213bba354d[m
+Merge: ab0bd8e75 c5a399a0b
+Author: manhnd3-07 <71371721+manhnd3-07@users.noreply.github.com>
+Date:   Sun Nov 8 15:57:26 2020 +0700
+
+    Merge pull request #24 from manhnd3-07/manhnd
+    
+    edit
+
+[33mcommit c5a399a0bd40dc42abc908437f2ecaf4a1eedd21[m
+Author: manhnd3 <manhnd3@vnext.com.vn>
+Date:   Sun Nov 8 15:56:24 2020 +0700
+
+    edit
+
+[33mcommit ab0bd8e75e0717bbf23648f811915d13887a29b1[m
+Merge: 976ade6cd e25f4df40
+Author: longtvph08895 <57783272+longtvph08895@users.noreply.github.com>
+Date:   Sun Nov 8 15:51:30 2020 +0700
+
+    Merge pull request #21 from manhnd3-07/long
+    
+    Long
+
+[33mcommit e25f4df40f01d5f44e527438da55c0661c8a6e70[m
+Merge: cf50359ac 976ade6cd
+Author: longtvph08895 <57783272+longtvph08895@users.noreply.github.com>
+Date:   Sun Nov 8 15:51:20 2020 +0700
+
+    Merge branch 'main' into long
+
+[33mcommit cf50359acac765a5cf8b51fbf1f364fef6ccceab[m
+Author: longtvph08895 <57783272+longtvph08895@users.noreply.github.com>
+Date:   Sun Nov 8 15:49:21 2020 +0700
+
+    edit
+
+[33mcommit 42675dc651338e85d1c0ca997848448661473b57[m
+Author: tiennhph <tiennhph08978@fpt.edu.vn>
+Date:   Sun Nov 8 11:07:59 2020 +0700
+
+    edit
+
+[33mcommit b1a45553f513601d2cce9e91e11f8ecca93edf55[m
+Merge: e8919f
