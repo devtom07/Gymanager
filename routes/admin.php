56cec8b555961ba1837b30fa82215b95df35d25b
leduchoanh04062k <leduchoanh2k@gmail.com>
2020-11-30 22:45:10 +0700

    dich vu

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 use Illuminate\Support\Facades\Route;
 
@@ -139,7 +136,19 @@ Route::get('/logout','AuthurController@logout')->name('logout');
              ->name('customer.update');
          Route::get('xoakhachhang/{id}','CustomerController@destroy')
              ->name('customer.delete');
-       //pt-level
+       //Dịch vụ
+       Route::get('/dich-vu','Dichvu\ServiceController@index')
+       ->name('service');
+       Route::get('/them-dich-vu','Dichvu\ServiceController@create')
+       ->name('service.add');
+       Route::post('/them-dich-vu','Dichvu\ServiceController@store')
+       ->name('service.store');
+       Route::get('/edit_dichvu/{id}','Dichvu\ServiceController@edit')
+       ->name('service.edit');
+   Route::post('/edit_dichvu/{id}','Dichvu\ServiceController@update')
+       ->name('service.update');
+   Route::get('/delete_dichvu/{id}','Dichvu\ServiceController@destroy')
+       ->name('service.delete');
 
 
 
