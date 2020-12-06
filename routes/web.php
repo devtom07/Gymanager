39d1ba5b18e68972315d6d1810f9de6756b2e9a5
tiennhph <tiennhph08978@fpt.edu.vn>
2020-12-06 17:38:50 +0700

    up_tien

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+// dao dien customers
+
+
+Route::get('/khach-hang','CustomerInterface\HomeController@index');
+Route::get('/login-khach-hang','CustomerInterface\LoginController@index');
+Route::get('/profile-khach-hang','CustomerInterface\HomeController@profile');
+
+
 require_once 'admin.php';
 require_once 'fontend.php';
