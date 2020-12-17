326164052f29c6f300aa574bbf458ed7e104d64b
leduchoanh04062k <leduchoanh2k@gmail.com>
2020-12-17 15:08:03 +0700

    full

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -9,15 +9,7 @@ class Package extends Model
     protected $table = 'packages';
     protected $primaryKey = 'id';
     protected $guarded =[];
-<<<<<<< HEAD
     public function hymnal(){
         return $this->hasOne(Hymnal::class, 'id', 'id_catap');
     }
 }
-=======
-
-    public function hymnal(){
-        return $this->hasOne(Hymnal::class, 'id', 'id_catap');
-    }
-}
->>>>>>> 6384bb357b7ad29d653c5497975eea14bfdd40f5
